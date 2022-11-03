3132e9b191a07540287dbe9e84e362dae5179ee2
Vladimir Obrizan <volodymyr.obrizan@gmail.com>
2022-11-03 15:48:51 +0200

    Replaced total_discount with query.

--- shopping_cart.py
+++ shopping_cart.py
@@ -18,22 +18,18 @@ class ShoppingCart:
 
     def description(self) -> str:
         """Gets a text representation of a cart."""
-        total_discount: float = 0.0
-
         # Print header.
         result: str = "Cart contents:\n"
 
         for entry in self.entries:
-            total_discount += entry.discount()
-
             # Print each cart entry line.
             result += f"* {entry.product.name} X {entry.qty} = {round(entry.price(), 2)} "
             result += f"(discount {round(entry.discount(), 2)})\n"
 
         # Print footer.
         result += f"Total cost: {round(self.get_total_cost(), 2)}\n"
-        result += f"Discount: {round(total_discount, 2)}\n"
-        result += f"Final cost: {round(self.get_total_cost()-total_discount, 2)}\n"
+        result += f"Discount: {round(self.get_total_discount(), 2)}\n"
+        result += f"Final cost: {round(self.get_total_cost()-self.get_total_discount(), 2)}\n"
 
         return result
 
@@ -42,3 +38,9 @@ class ShoppingCart:
         for entry in self.entries:
             total_cost += entry.price()
         return total_cost
+
+    def get_total_discount(self) -> float:
+        total_discount: float = 0.0
+        for entry in self.entries:
+            total_discount += entry.discount()
+        return total_discount
