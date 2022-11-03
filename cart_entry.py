6eccb64014c55e273138a645ea6149969b4bb101
Vladimir Obrizan <volodymyr.obrizan@gmail.com>
2022-11-03 15:00:00 +0200

    Moved discount method.

--- cart_entry.py
+++ cart_entry.py
@@ -29,3 +29,24 @@ class CartEntry:
         self.product = product
         self.qty = qty
         self.discount_type = discount_type
+
+    def discount(self) -> float:
+        entry_discount: float = 0.0
+        match self.discount_type:
+            case DiscountType.NO_DISCOUNT:
+                # Do nothing.
+                pass
+            case DiscountType.COUPON_MINUS_TEN:
+                # Make sure we don't give discount higher than the cost.
+                # E.g. cost is 9, and the coupon is 10.
+                entry_discount = (
+                    10.0
+                    if self.product.price * self.qty >= 10.0
+                    else self.product.price * self.qty
+                )
+            case DiscountType.FIVE_OR_MORE_MINUS_5_PERCENT:
+                if self.qty >= 5:
+                    entry_discount = self.product.price * self.qty * 0.05
+            case DiscountType.EACH_THIRD_FREE:
+                entry_discount = (self.qty // 3) * self.product.price
+        return entry_discount
--- shopping_cart.py
+++ shopping_cart.py
@@ -27,8 +27,8 @@ class ShoppingCart:
         for entry in self.entries:
             total_cost += entry.product.price * entry.qty
 
-            # Calculate discount_for_entry.
-            entry_discount = self.discount_for_entry(entry)
+            # Calculate discount.
+            entry_discount = entry.discount()
 
             total_discount += entry_discount
 
@@ -42,24 +42,3 @@ class ShoppingCart:
         result += f"Final cost: {round(total_cost-total_discount, 2)}\n"
 
         return result
-
-    def discount_for_entry(self, entry: CartEntry) -> float:
-        entry_discount: float = 0.0
-        match entry.discount_type:
-            case DiscountType.NO_DISCOUNT:
-                # Do nothing.
-                pass
-            case DiscountType.COUPON_MINUS_TEN:
-                # Make sure we don't give discount_for_entry higher than the cost.
-                # E.g. cost is 9, and the coupon is 10.
-                entry_discount = (
-                    10.0
-                    if entry.product.price * entry.qty >= 10.0
-                    else entry.product.price * entry.qty
-                )
-            case DiscountType.FIVE_OR_MORE_MINUS_5_PERCENT:
-                if entry.qty >= 5:
-                    entry_discount = entry.product.price * entry.qty * 0.05
-            case DiscountType.EACH_THIRD_FREE:
-                entry_discount = (entry.qty // 3) * entry.product.price
-        return entry_discount
